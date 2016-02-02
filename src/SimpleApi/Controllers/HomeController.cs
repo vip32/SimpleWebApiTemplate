796fbb653eb4dd6b84c4048571ac4368e66cab8e
vip32 <mini32@gmail.com>
2016-02-03 00:40:40 +0100

    init

---
+++ src/SimpleApi/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace ProjectAPI.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return new ResponseMessageResult(new HttpResponseMessage
+            {
+                Content = new StringContent("Welcome to my API!")
+            });
+        }
+    }
+}
---
+++ src/SimpleApiConsoleHost/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+using ProjectAPI;
+
+namespace ConsoleHostProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:12345"))
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
